9c44fcb4900e6e90c54ec3a42d6c30ee3baea656
Northstrider <Dreadkiller@gmx.de>
2013-06-01 19:12:03 +0200

    Core/Spells: fixed druids effloresence talent

---
+++ sql/updates/world/08_spell_effloresence.sql
@@ -0,0 +1,16 @@
+﻿
+/*
+Author : Northstrider
+Descr. : druid efflorescence
+Ticket :
+*/
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = 81262;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(81262,'spell_dru_efflorescence');
+
+DELETE FROM `spell_proc_event` WHERE `entry` IN (34151, 81274, 81275);
+INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `SpellFamilyMask1`) VALUES
+('34151','7','2'),
+('81274','7','2'),
+('81275','7','2');
