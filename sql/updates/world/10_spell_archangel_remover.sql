66357768bf351774dcf3fb15afc66cc10300e6b2
john2308 <gianni.elio@hotmail.it>
2013-06-02 16:12:06 +0200

    Core/Talents: Fixed Harnessed Shadows extra Shadow Orb Proc effect Core/Talents: Fixed Shadow Orb Marker DB/Spells: Better update on Archangel Marker remover DB/Spells: Implement Shadow Orb Marker remover
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/10_spell_archangel_remover.sql
+++ sql/updates/world/10_spell_archangel_remover.sql
@@ -1,3 +1,10 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '-87117';
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '-87118';
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES (-81660, -87154, 'Evangelism -> Remove Archangel Trigger');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES (-81661, -87154, 'Evangelism -> Remove Archangel Trigger');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES (-87117, -94709, 'Dark Evangelism -> Remove Archangel Trigger');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES (-87118, -87154, 'Dark Evangelism -> Remove Archangel Trigger');
+
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=-87118 AND `spell_effect`=-94709 AND `type`=0;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=-87117 AND `spell_effect`=-94709 AND `type`=0;
 DELETE FROM `spell_linked_spell` WHERE `spell_trigger`=-81661 AND `spell_effect`=-94709 AND `type`=0;
--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6107,6 +6107,54 @@ void AuraEffect::HandlePeriodicDamageAurasTick(Unit* target, Unit* caster) const
 						}
 					}
 				}
+				// Shadow Orb Proc 
+				if (AuraEffect* aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_PRIEST, 4941, 0))
+				{
+					int32 chance = 10;
+					// Harnessed Shadows 
+					if (AuraEffect* harnessedShadows = caster->GetAuraEffect(SPELL_AURA_ADD_FLAT_MODIFIER, SPELLFAMILY_PRIEST, 554, 0))
+						chance += harnessedShadows->GetAmount();
+					
+					// Cast extra Shadow Orb if proc chance
+					if (roll_chance_i(chance))
+						caster->CastSpell(caster, 77487, true);
+					
+					// Shadow Orbs!
+					if (Aura const* shadowOrbs = caster->GetAura(77487))
+					{
+						if (shadowOrbs->GetStackAmount() >= 3)
+						{
+							if (!caster->HasAura(93683))
+								caster->AddAura(93683, caster);
+						}
+					}
+				}
+				break;
+			}
+			case 589: // Shadow Word: Pain
+			{
+				// Shadow Orb Proc 
+				if (AuraEffect* aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_PRIEST, 4941, 0))
+				{
+					int32 chance = 10;
+					// Harnessed Shadows 
+					if (AuraEffect* harnessedShadows = caster->GetAuraEffect(SPELL_AURA_ADD_FLAT_MODIFIER, SPELLFAMILY_PRIEST, 554, 0))
+						chance += harnessedShadows->GetAmount();
+					
+					// Cast extra Shadow Orb if proc chance
+					if (roll_chance_i(chance))
+						caster->CastSpell(caster, 77487, true);
+
+					// Shadow Orbs Marker
+					if (Aura const* shadowOrbs = caster->GetAura(77487))
+					{
+						if (shadowOrbs->GetStackAmount() >= 3)
+						{
+							if (!caster->HasAura(93683))
+								caster->AddAura(93683, caster);
+						}
+					}
+				}
 				break;
 			}
         }
