aaa9bac0ec0e7fc281ca0aa16a814ec022f1bb77
john2308 <gianni.elio@hotmail.it>
2013-07-06 16:52:23 +0200

    DB/Talents: Add SQL for Nature's Grace DB/Talents: Fixed Moonkin Form haste effect
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/60_talent_nature_s_grace.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE  `entry`=61345;
+DELETE FROM `spell_proc_event` WHERE  `entry`=61346;
+DELETE FROM `spell_proc_event` WHERE  `entry`=16880;
---
+++ sql/updates/world/61_talent_moonkin_form.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (24858, 24907, 0, 'Moonkin Form -> Apply Moonkin Aura');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (-24858, -24907, 0, 'Moonkin Form -> Remove Moonkin Aura');
