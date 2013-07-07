5bbaf2b512dcf5250b15b33b901c4bcdd7dfc2a3
john2308 <gianni.elio@hotmail.it>
2013-07-07 23:14:34 +0200

    Core/Spells: Remove deprecated fix for Drain Soul
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -6387,12 +6387,6 @@ void AuraEffect::HandlePeriodicDamageAurasTick(Unit* target, Unit* caster) const
                 damage += (damage+1)/2;           // +1 prevent 0.5 damage possible lost at 1..4 ticks
             // 5..8 ticks have normal tick damage
         }
-        // There is a Chance to make a Soul Shard when Drain soul does damage
-        if (GetSpellInfo()->SpellFamilyName == SPELLFAMILY_WARLOCK && (GetSpellInfo()->SpellFamilyFlags[0] & 0x00004000))
-        {
-            if (caster->GetTypeId() == TYPEID_PLAYER && caster->ToPlayer()->isHonorOrXPTarget(target))
-                caster->CastSpell(caster, 95810, true, 0, this);
-        }
 
         if (caster->GetTypeId() == TYPEID_PLAYER)
         {
