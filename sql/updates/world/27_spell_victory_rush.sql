a02edca74e3b1247d3aa99a05b197ca9911df408
john2308 <gianni.elio@hotmail.it>
2013-07-17 14:02:32 +0200

    DB/Talents: Add SQL support for Victory Rush cooldown
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/27_spell_victory_rush.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = '34428';
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (34428, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
