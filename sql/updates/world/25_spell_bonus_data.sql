daaf01aa199cdb381bffc0b54b3bf82800152891
Northstrider <Dreadkiller@gmx.de>
2013-07-17 12:04:31 +0200

    DB/Spells: added 2 entries for storm and primal strike to prevent  sp exploit

---
+++ sql/updates/world/25_spell_bonus_data.sql
@@ -0,0 +1,5 @@
+﻿
+REPLACE INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(17364, 0, 0, 0, 0, 'Shaman - Storm Strike'),
+(73899, 0, 0, 0, 0, 'Shaman - Primal Strike'),
+(60103, 0, 0, 0, 0, 'Shaman - Lava Lash');
