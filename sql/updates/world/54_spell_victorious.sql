6b78504367bfab8b37c63bf44efa28c84ff3bd96
Northstrider <Dreadkiller@gmx.de>
2013-07-28 20:34:10 +0200

    DB/Spells: proper aura removal for victorious

---
+++ sql/updates/world/54_spell_victorious.sql
@@ -0,0 +1,5 @@
+﻿-- Victorious
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = 34428;
+INSERT INTO `spell_linked_spell` VALUES
+(34428, -32216, 1, 'Victorious - Delete Aura'),
+(34428, -82368, 1, 'Impending Victory - Delete Aura');
