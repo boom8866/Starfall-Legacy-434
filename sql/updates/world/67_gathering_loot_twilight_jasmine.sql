04c9c0043bb7f7d935502a369e6c9df186721901
john2308 <gianni.elio@hotmail.it>
2013-08-05 12:11:27 +0200

    DB/Profession: Fixed Twilight Jasmine herb gathering loot
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/67_gathering_loot_twilight_jasmine.sql
@@ -0,0 +1,5 @@
+DELETE FROM `gameobject_loot_template` WHERE `entry` = '202751';
+UPDATE `gameobject_template` SET `data1`=202751 WHERE  `entry`=202751;
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (202751, 52987, 100, 1, 0, 2, 4);
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (202751, 52329, 39, 1, 0, 1, 3);
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES (202751, 63122, 10, 1, 0, 1, 1);
