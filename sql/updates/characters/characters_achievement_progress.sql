2cd96b63646e05702e489a10367aebd8bf1d59bd
john2308 <gianni.elio@hotmail.it>
2013-08-27 16:06:27 +0200

    DB/SQL: Move some .sql updates into right folders
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/characters/characters_achievement_progress.sql
+++
@@ -1,3 +0,0 @@
-﻿
-ALTER TABLE `character_achievement_progress` CHANGE `counter` `counter` bigint(20) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE `guild_achievement_progress` CHANGE `counter` `counter` bigint(20) unsigned NOT NULL DEFAULT '0';
