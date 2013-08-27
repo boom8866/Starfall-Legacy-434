8e3a3af808938e6fd2c4465f6415f4fbf1d0cb3c
Northstrider <Dreadkiller@gmx.de>
2013-08-27 17:51:55 +0200

    DB/DS: removed some double / unneeded spawns

---
+++ sql/updates/world/88_world_dragonsoul_cleanup.sql
@@ -0,0 +1,26 @@
+﻿-- Deletes creature Target Dummy (id: 56126, guid: 786136) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786136; DELETE FROM creature_addon WHERE guid = 786136;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786374) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786374; DELETE FROM creature_addon WHERE guid = 786374;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786376) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786376; DELETE FROM creature_addon WHERE guid = 786376;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786239) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786239; DELETE FROM creature_addon WHERE guid = 786239;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786021) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786021; DELETE FROM creature_addon WHERE guid = 786021;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786373) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786373; DELETE FROM creature_addon WHERE guid = 786373;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786240) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786240; DELETE FROM creature_addon WHERE guid = 786240;
+
+-- Deletes creature Target Dummy (id: 56126, guid: 786145) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786145; DELETE FROM creature_addon WHERE guid = 786145;
+
+-- Deletes creature Twilight Assaulter (id: 56251, guid: 786585) in zone: 0, area: 0
+DELETE FROM `creature` WHERE `guid` = 786585; DELETE FROM creature_addon WHERE guid = 786585;
