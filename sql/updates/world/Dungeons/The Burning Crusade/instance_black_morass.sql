0ac119347e4b8831f7c2e3a8841955643756eac6
john2308 <gianni.elio@hotmail.it>
2013-08-31 16:05:23 +0200

    DB/Dungeons: Add right sql for Black Morass
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/Dungeons/The Burning Crusade/instance_black_morass.sql
+++ sql/updates/world/Dungeons/The Burning Crusade/instance_black_morass.sql
@@ -1,2 +1,5 @@
 DELETE FROM `access_requirement` WHERE  `mapId`=269 AND `difficulty`=0;
 DELETE FROM `access_requirement` WHERE  `mapId`=269 AND `difficulty`=1;
+INSERT INTO `access_requirement` (`mapId`, `difficulty`, `level_min`, `level_max`, `item`, `item2`, `quest_done_A`, `quest_done_H`, `completed_achievement`, `quest_failed_text`, `comment`) VALUES 
+(269, 0, 66, 0, 0, 0, 0, 0, 0, '', 'Caverns Of Time,Black Morass (Entrance)'),
+(269, 1, 70, 0, 0, 0, 0, 0, 0, '', 'Caverns Of Time,Black Morass (Entrance)');
