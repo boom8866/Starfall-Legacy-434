30598cd979ba981f0630c1fb8482889fc08a2e06
Northstrider <Dreadkiller@gmx.de>
2013-09-09 19:51:48 +0200

    Core/Quests: implement quest start type. Note: * queststarttype = 0 - normal quest * queststarttype = 1 + autoaccept = quest without questgiver

--- src/server/game/Quests/QuestDef.cpp
+++ src/server/game/Quests/QuestDef.cpp
@@ -166,7 +166,8 @@ Quest::Quest(Field* questRecord)
     for (int i = 0; i < QUEST_EMOTE_COUNT; ++i)
         OfferRewardEmoteDelay[i] = questRecord[168+i].GetInt32();
 
-    // int32 WDBVerified = questRecord[172].GetInt32();
+    QuestStartType = questRecord[172].GetUInt32();
+    // int32 WDBVerified = questRecord[173].GetInt32();
 
     if (SpecialFlags & QUEST_SPECIAL_FLAGS_AUTO_ACCEPT)
         Flags |= QUEST_FLAGS_AUTO_ACCEPT;
