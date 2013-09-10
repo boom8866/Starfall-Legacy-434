dfac4a5f792d55a0e65fc413c1c85a1ac1c3240e
john2308 <gianni.elio@hotmail.it>
2013-09-10 15:32:30 +0200

    DB/SQL: Fix world_quest_start_type.sql
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/92_world_quest_start_type.sql
+++ sql/updates/world/92_world_quest_start_type.sql
@@ -1,3 +1,1 @@
-﻿
-ALTER TABLE `quest_template` DROP COLUMN `QuestStartType`;
 ALTER TABLE `quest_template` ADD COLUMN `QuestStartType` mediumint(8) UNSIGNED NOT NULL DEFAULT '0' AFTER `OfferRewardEmoteDelay4`;
