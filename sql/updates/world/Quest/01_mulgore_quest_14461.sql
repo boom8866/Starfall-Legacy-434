495ef2492b0b828711e1193361f5ccd7c5a16323
john2308 <gianni.elio@hotmail.it>
2013-09-15 16:06:21 +0200

    DB/SQL: Update creature position for quest 14461
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/Quest/01_mulgore_quest_14461.sql
+++ sql/updates/world/Quest/01_mulgore_quest_14461.sql
@@ -2,9 +2,9 @@ DELETE FROM `creature` WHERE  `id`=36727;
 DELETE FROM `creature` WHERE  `id`=37156;
 DELETE FROM `creature` WHERE  `id`=37155;
 INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
-(36727, 1, 1, 1, 0, 0, -3357.96, -232.686, 62.9092, 3.00917, 300, 0, 0, 274, 0, 0, 0, 0, 0),
-(37156, 1, 1, 1, 0, 0, -3406.48, -264.5007, 62.6694, 0.16686, 300, 0, 0, 274, 0, 0, 0, 0, 0),
-(37155, 1, 1, 1, 0, 0, -3391.01, -210.610, 62.7029, 4.99345, 300, 0, 0, 274, 0, 0, 0, 0, 0);
+(36727, 1, 1, 1, 0, 0, -3358,21, -232,444, 63,0433, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0),
+(37156, 1, 1, 1, 0, 0, -3406,35, -265,036, 62,7771, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0),
+(37155, 1, 1, 1, 0, 0, -3391,44, -210,651, 62,8214, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0);
 
 UPDATE `creature_template` SET `ScriptName`='npc_trough' WHERE  `entry`=36727;
 UPDATE `creature_template` SET `ScriptName`='npc_trough' WHERE  `entry`=37155;
