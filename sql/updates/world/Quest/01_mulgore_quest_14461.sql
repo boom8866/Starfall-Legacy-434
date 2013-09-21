873c23decc2a8ace156fb3cebebfc6ca85d19793
Northstrider <Dreadkiller@gmx.de>
2013-09-21 09:58:37 +0200

    DB/SQL: *slab john*

--- sql/updates/world/Quest/01_mulgore_quest_14461.sql
+++ sql/updates/world/Quest/01_mulgore_quest_14461.sql
@@ -1,17 +1,17 @@
-DELETE FROM `creature` WHERE  `id`=36727;
-DELETE FROM `creature` WHERE  `id`=37156;
-DELETE FROM `creature` WHERE  `id`=37155;
-INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES 
-(36727, 1, 1, 1, 0, 0, -3358,21, -232,444, 63,0433, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0),
-(37156, 1, 1, 1, 0, 0, -3406,35, -265,036, 62,7771, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0),
-(37155, 1, 1, 1, 0, 0, -3391,44, -210,651, 62,8214, 0, 300, 0, 0, 274, 0, 0, 0, 0, 0);
+DELETE FROM `creature` WHERE `id` In (36727, 37156, 37155);
+SET @CGUID := 786684;
+DELETE FROM `creature` WHERE `guid` BETWEEN @CGUID+0 AND @CGUID+2;
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `MovementType`) VALUES
+(@CGUID+0,37156,1,1,1,-3406.3501,-265.036011,62.7770996,0,300,3,1),
+(@CGUID+1,36727,1,1,1,-3358.20996,-232.444,63.0433006,0,300,3,1),
+(@CGUID+2,37155,1,1,1,-3391.43994,-210.651001,62.8213997,0,300,3,1);
 
-UPDATE `creature_template` SET `ScriptName`='npc_trough' WHERE  `entry`=36727;
-UPDATE `creature_template` SET `ScriptName`='npc_trough' WHERE  `entry`=37155;
-UPDATE `creature_template` SET `ScriptName`='npc_trough' WHERE  `entry`=37156;
+UPDATE `creature_template` SET `ScriptName`= 'npc_trough' WHERE `entry`= 36727;
+UPDATE `creature_template` SET `ScriptName`= 'npc_trough' WHERE `entry`= 37155;
+UPDATE `creature_template` SET `ScriptName`= 'npc_trough' WHERE `entry`= 37156;
 
-DELETE FROM `conditions` WHERE `SourceEntry` = '69228';
-INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
-(13, 0, 69228, 0, 0, 31, 0, 3, 37156, 0, 0, 0, 0, '', 'Throw Torch for NPC: 37156'),
-(13, 0, 69228, 0, 0, 31, 0, 3, 37155, 0, 0, 0, 0, '', 'Throw Torch for NPC: 37155'),
-(13, 0, 69228, 0, 0, 31, 0, 3, 36727, 0, 0, 0, 0, '', 'Throw Torch for NPC: 36727');
+DELETE FROM `conditions` WHERE `SourceEntry`= 69228;
+INSERT INTO conditions (SourceTypeOrReferenceId, SourceGroup, SourceEntry, SourceId, ElseGroup, ConditionTypeOrReference, ConditionTarget, ConditionValue1, ConditionValue2, ConditionValue3, NegativeCondition, ErrorType, ScriptName, Comment) VALUES
+(13, 0, 69228, 0, 0, 31, 0, 3, 37156, 0, 0, 0, '', 'Throw Torch for NPC: 37156'),
+(13, 0, 69228, 0, 0, 31, 0, 3, 37155, 0, 0, 0, '', 'Throw Torch for NPC: 37155'),
+(13, 0, 69228, 0, 0, 31, 0, 3, 36727, 0, 0, 0, '', 'Throw Torch for NPC: 36727');
