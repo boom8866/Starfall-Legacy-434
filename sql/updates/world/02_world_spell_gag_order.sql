b90595d9ac01feb436124422c2e0ccdc14b6938c
TMM <nuna.vlad@yahoo.com>
2013-09-21 19:33:40 +0100

    Correct SQL errors sorry i was to excited :D
    Signed-off-by: TMM <nuna.vlad@yahoo.com>

--- sql/updates/world/02_world_spell_gag_order.sql
+++ sql/updates/world/02_world_spell_gag_order.sql
@@ -1,4 +1,4 @@
 -- Gag Order
-UPDATE `spell_proc_event` SET `SpellFamilyMask0`='8', `procFlags`='87376' WHERE `entry IN('12311','12958');
+UPDATE `spell_proc_event` SET `SpellFamilyMask0`='8', `procFlags`='87376' WHERE `entry` IN('12311','12958');
 UPDATE `spell_proc_event` SET `CustomChance`='50' WHERE `entry` = '12311';
 UPDATE `spell_proc_event` SET `CustomChance`='100' WHERE `entry` = '12958';
--- sql/updates/world/06_world_cruelty.sql
+++ sql/updates/world/06_world_cruelty.sql
@@ -1,5 +1,5 @@
 -- Cruelty
-DELETE FROM `` WHERE `spell_id` IN(12852,12320);
+DELETE FROM `spell_script_names` WHERE `spell_id` IN(12852,12320);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 ('12852', 'spell_warr_cruelty'),
 ('12320', 'spell_warr_cruelty');
