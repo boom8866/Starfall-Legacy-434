9b3191f25e0b91b9bf171f7a9a90d7cf264ab101
Northstrider <Dreadkiller@gmx.de>
2013-09-28 19:44:41 +0200

    Scripts/Elwynnforest: some ai codes for the hogger quest. not done yet.

--- src/server/scripts/EasternKingdoms/elwynnforest.cpp
+++ src/server/scripts/EasternKingdoms/elwynnforest.cpp
@@ -285,9 +285,189 @@ public:
     }
 };
 
+enum HoggerSpells
+{
+    SPELL_SUMMON_MINIONS    = 87366,
+    SPELL_EVIL_SLICE        = 87337,
+    SPELL_EAT               = 87351,
+    SPELL_UPSET_STOMACH     = 87352,
+    SPELL_TELEPORT_VISUAL   = 41232,
+};
+
+enum HoggerNpcs
+{
+    NPC_HOGGER_MINION       = 46932,
+    NPC_GENERAL_MARCUS      = 46942,
+    NPC_MAGINOR_DUMAS       = 46940,
+    NPC_ANDROMATH           = 46941,
+    NPC_RAGAMUFFIN          = 46943,
+};
+
+enum HoggerEvents
+{
+    EVENT_SLICE = 1,
+    EVENT_MOVE_EAT,
+    EVENT_EAT,
+    EVENT_MOVE_CAPTURE,
+    EVENT_GRAWL,
+    EVENT_TALK_GENERAL_1,
+};
+
+enum HoggerTexts
+{
+    // Hogger
+    SAY_HELP        = 1,
+    SAY_DONT_HURT   = 2,
+    SAY_GRAWL       = 3,
+    SAY_NO          = 4,
+    // General
+    SAY_OUTRO_1     = 1,
+    SAY_OUTRO_2     = 2,
+    SAY_OUTRO_3     = 3,
+};
+
+// The elwynn hogger casts random spells until 50% hp. after that
+// he is going to heal himself by eating meat and getting stunned
+// after completing this. if the moves to his meat he summons 3 minions
+// at 1 hp hogger moves to a point and flees for not getting killed.
+// after that 46942 appears with 2 mages 46940(l) and 46941(r)
+// after the mismount  2 childrens 46943 appear behind the commander
+Position const HoggerPos[] = {-10134.947f, 669.700f, 35.802f, 0.0f};
+
+class npc_hogger : public CreatureScript
+{
+public:
+    npc_hogger() : CreatureScript("npc_hogger") { }
+
+    struct npc_hoggerAI : public ScriptedAI
+    {
+        npc_hoggerAI(Creature* creature) : ScriptedAI(creature)
+        {
+            finished = false;
+        }
+
+        EventMap events;
+        bool finished;
+
+        void Reset()
+        {
+            events.Reset();
+            finished = false;
+        }
+
+        void EnterCombat(Unit* who)
+        {
+            events.ScheduleEvent(EVENT_SLICE, 8000);
+        }
+
+        void JustDied(Unit* /*killer*/)
+        {
+        }
+
+        void DamageTaken(Unit* attacker, uint32& damage)
+        {
+            if (me->HealthBelowPctDamaged(1, damage) && !finished)
+            {
+                if (damage > me->GetHealth())
+                {
+                    damage = 0;
+                    finished = true;
+                    me->SetHealth(1);
+                    me->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE | UNIT_FLAG_NOT_SELECTABLE);
+                    me->SetWalk(true);
+                    me->AttackStop();
+                    me->SetReactState(REACT_PASSIVE);
+                    events.Reset();
+                    me->GetMotionMaster()->MovePoint(1, HoggerPos->GetPositionX(), HoggerPos->GetPositionY(), HoggerPos->GetPositionZ(), false);
+                    Talk(SAY_DONT_HURT);
+                }
+            }
+        }
+
+        void MovementInform(uint32 type, uint32 pointId)
+        {
+            switch (pointId)
+            {
+                case 1:
+                    events.ScheduleEvent(EVENT_MOVE_CAPTURE, 1000);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void UpdateAI(uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            events.Update(diff);
+
+            while (uint32 eventId = events.ExecuteEvent())
+            {
+                switch (eventId)
+                {
+                    case EVENT_SLICE:
+                        DoCastVictim(SPELL_EVIL_SLICE);
+                        events.ScheduleEvent(EVENT_SLICE, 8000);
+                        break;
+                    case EVENT_MOVE_CAPTURE:
+                    {
+                        events.ScheduleEvent(EVENT_GRAWL, 3000);
+                        std::list<Creature*> units;
+                        GetCreatureListWithEntryInGrid(units, me, NPC_GENERAL_MARCUS, 200.0f);
+                        for (std::list<Creature*>::iterator itr = units.begin(); itr != units.end(); ++itr)
+                        {
+                            (*itr)->SetWalk(true);
+                            (*itr)->SetPhaseMask(me->GetPhaseMask(), true);
+                            (*itr)->CastSpell((*itr), SPELL_TELEPORT_VISUAL);
+                            (*itr)->GetMotionMaster()->MovePoint(0, (*itr)->GetPositionX()+cos((*itr)->GetOrientation())*10, (*itr)->GetPositionY()+sin((*itr)->GetOrientation())*10, (*itr)->GetPositionZ(), false);
+                        }
+                        GetCreatureListWithEntryInGrid(units, me, NPC_ANDROMATH, 200.0f);
+                        for (std::list<Creature*>::iterator itr = units.begin(); itr != units.end(); ++itr)
+                        {
+                            (*itr)->SetWalk(true);
+                            (*itr)->SetPhaseMask(me->GetPhaseMask(), true);
+                            (*itr)->CastSpell((*itr), SPELL_TELEPORT_VISUAL);
+                            (*itr)->GetMotionMaster()->MovePoint(0, (*itr)->GetPositionX()+cos((*itr)->GetOrientation())*10, (*itr)->GetPositionY()+sin((*itr)->GetOrientation())*10, (*itr)->GetPositionZ(), false);
+                        }
+                        GetCreatureListWithEntryInGrid(units, me, NPC_MAGINOR_DUMAS, 200.0f);
+                        for (std::list<Creature*>::iterator itr = units.begin(); itr != units.end(); ++itr)
+                        {
+                            (*itr)->SetWalk(true);
+                            (*itr)->SetPhaseMask(me->GetPhaseMask(), true);
+                            (*itr)->CastSpell((*itr), SPELL_TELEPORT_VISUAL);
+                            (*itr)->GetMotionMaster()->MovePoint(0, (*itr)->GetPositionX()+cos((*itr)->GetOrientation())*10, (*itr)->GetPositionY()+sin((*itr)->GetOrientation())*10, (*itr)->GetPositionZ(), false);
+                        }
+                        break;
+                    }
+                    case EVENT_GRAWL:
+                        Talk(SAY_GRAWL);
+                        events.ScheduleEvent(EVENT_TALK_GENERAL_1, 3000);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            DoMeleeAttackIfReady();
+        }
+
+        void EnterEvadeMode()
+        {
+            me->GetMotionMaster()->MoveTargetedHome();
+            me->DespawnCreaturesInArea(NPC_HOGGER_MINION, 200.0f);
+        }
+    };
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_hoggerAI (creature);
+    }
+};
+
 void AddSC_elwynn_forest()
 {
     new npc_stormwind_infantry();
     new npc_blackrock_battle_worg();
     new npc_injured_soldier();
+    new npc_hogger();
 }
