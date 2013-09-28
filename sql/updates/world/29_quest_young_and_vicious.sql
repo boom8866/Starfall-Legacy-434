de7f6317c481abefe76df287c4a6ba3b87ca8e04
Northstrider <Dreadkiller@gmx.de>
2013-09-28 23:52:52 +0200

    *slab john once more

--- sql/updates/world/29_quest_young_and_vicious.sql
+++ sql/updates/world/29_quest_young_and_vicious.sql
@@ -1,6 +1,6 @@
 UPDATE `creature_template` SET `VehicleId`=1327 WHERE  `entry`=37989;
 DELETE FROM `creature_text` WHERE `entry`=37989;
-INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (37989,0,0,'Swiftclaw isn't stopping! Steer him back to the raptor pens near Darkspear Hold.',41,0,100,0,0,0,'Comment');
+INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES (37989,0,0,'Swiftclaw isn\'t stopping! Steer him back to the raptor pens near Darkspear Hold.',41,0,100,0,0,0,'Comment');
 DELETE FROM `spell_script_names` WHERE `spell_id` = '70927';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (70927, 'spell_bloodtalon_lasso');
 DELETE FROM `areatrigger_involvedrelation` WHERE `id` = '5675';
