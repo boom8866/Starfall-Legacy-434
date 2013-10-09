e82f65fff1393511deb813ee5bed773d855226ac
TMM <nuna.vlad@yahoo.com>
2013-10-09 09:31:43 +0100

    Cleanup errors on sql files
    Signed-off-by: TMM <nuna.vlad@yahoo.com>

--- sql/updates/world/50_quest_the_public_servant.sql
+++ sql/updates/world/50_quest_the_public_servant.sql
@@ -5,7 +5,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 
 DELETE FROM `creature_text` WHERE `entry`=41671;
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`language`,`probability`,`emote`,`duration`,`sound`,`comment`) VALUES
-(41671,0,0,'You're the best thing I've seen since the troggs showed up!.',12,0,100,0,0,0,'Comment'),
+(41671,0,0,'You\'re the best thing I\'ve seen since the troggs showed up!.',12,0,100,0,0,0,'Comment'),
 (41671,0,1,'I thought I would die down here.',12,0,100,0,0,0,'Comment'),
 (41671,0,2,'I thought they were going to tear me apart!.',12,0,100,0,0,0,'Comment'),
-(41671,0,3,'I can't wait to get home to my family. Thank you, stranger.',12,0,100,0,0,0,'Comment');
+(41671,0,3,'I can\'t wait to get home to my family. Thank you, stranger.',12,0,100,0,0,0,'Comment');
