315175f4a78d6cd637cda79be1e0fee1ca01d98a
john2308 <gianni.elio@hotmail.it>
2013-10-14 17:57:14 +0200

    DB/SQL: Corrections on last SQL commit
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/87_quest_call_down_the_thunder.sql
+++ sql/updates/world/87_quest_call_down_the_thunder.sql
@@ -29,5 +29,3 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCE
 UPDATE gameobject_template SET AIName="SmartObjectAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,64,0,100,0,0,0,0,0,45,0,1,0,0,0,0,10,188223,33041,0,0.0,0.0,0.0,0.0,"On Gossip Hello - Set Data 1 to Aetherion");
-
-UPDATE `creature_template` SET `minlevel`=16, `maxlevel`=16, `faction_A`=2146, `faction_H`=2146, `mindmg`=45, `maxdmg`=90, `lootid`=33041 WHERE  `entry`=33042;
