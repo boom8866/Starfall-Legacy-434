83b1a1ddc589dce3617972149bd4465ce473192e
Northstrider <Dreadkiller@gmx.de>
2013-10-17 21:22:47 +0200

    Core/Stats: removed 2 wrong changes

--- src/server/game/Entities/Unit/StatSystem.cpp
+++ src/server/game/Entities/Unit/StatSystem.cpp
@@ -659,7 +659,6 @@ void Player::UpdateMeleeHitChances()
 {
     m_modMeleeHitChance = (float)GetTotalAuraModifier(SPELL_AURA_MOD_HIT_CHANCE);
     m_modMeleeHitChance += GetRatingBonusValue(CR_HIT_MELEE);
-    SetStatFloatValue(PLAYER_FIELD_UI_HIT_MODIFIER, m_modMeleeHitChance);
 }
 
 void Player::UpdateRangedHitChances()
@@ -672,7 +671,6 @@ void Player::UpdateSpellHitChances()
 {
     m_modSpellHitChance = (float)GetTotalAuraModifier(SPELL_AURA_MOD_SPELL_HIT_CHANCE);
     m_modSpellHitChance += GetRatingBonusValue(CR_HIT_SPELL);
-    SetStatFloatValue(PLAYER_FIELD_UI_SPELL_HIT_MODIFIER, m_modSpellHitChance);
 }
 
 void Player::UpdateAllSpellCritChances()
