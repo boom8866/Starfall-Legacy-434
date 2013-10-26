190f81eb88b613777b2612e2dc2f5c31dd8674cd
john2308 <gianni.elio@hotmail.it>
2013-10-26 14:46:57 +0200

    DB/Spells: Fixed Ring of Frost 3 seconds immunity DB/Spells: Remove some unneded coefficients for hunter
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/109_spell_ring_of_frost_immunity.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_linked_spell` WHERE `spell_trigger` = '-82691';
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES
+(-82691, 91264, 'Ring of Frost Immunity');
