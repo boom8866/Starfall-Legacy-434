4eba4edb21066efb67f31ab7ae7fa796cb6bb616
john2308 <gianni.elio@hotmail.it>
2014-01-22 12:54:57 +0100

    DB/Event: Fixed Lunar Festival Harbinger gossip
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/33_world_event_lunar_festival.sql
+++ sql/updates/world/33_world_event_lunar_festival.sql
@@ -1837,3 +1837,19 @@ UPDATE `gameobject` SET `phaseMask` = '1' WHERE `guid` IN
 734447,
 731634,
 731824);
+
+DELETE FROM `gossip_menu_option` WHERE `menu_id`=6918 AND `id`=1;
+
+DELETE FROM `conditions` WHERE `SourceGroup`=6918;
+INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `SourceId`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionTarget`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `NegativeCondition`, `ErrorType`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES
+(15, 6918, 0, 0, 0, 8, 0, 8867, 0, 0, 0, 0, 0, '', 'Lunar Invitation - Show gossip only if quest 8867 is Rewarded');
+
+-- Lunar Festival Harbinger
+SET @ENTRY := 15895;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,62,0,100,0,6918,0,0,0,72,0,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Gossip Select - Close Gossip"),
+(@ENTRY,@SOURCETYPE,1,0,62,0,100,0,6918,0,0,0,56,21711,1,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Gossip Select - Add Item");
