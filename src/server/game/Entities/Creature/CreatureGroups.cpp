9a85ce45f0b2a108a9f7b55443bc0bbc6d4b6ebc
ZackTsuna <Dreadkiller@gmx.de>
2014-01-28 22:25:02 +0100

    Core/Creatures: fixed formation walk state. reason is a missing movement flag update on initialization

--- src/server/game/Entities/Creature/CreatureGroups.cpp
+++ src/server/game/Entities/Creature/CreatureGroups.cpp
@@ -216,6 +216,8 @@ void CreatureGroup::LeaderMoveTo(float x, float y, float z)
     if (!m_leader)
         return;
 
+    m_leader->SetWalk(true);
+
     float pathangle = atan2(m_leader->GetPositionY() - y, m_leader->GetPositionX() - x);
 
     for (CreatureGroupMemberType::iterator itr = m_members.begin(); itr != m_members.end(); ++itr)
