9f138c3223e7b1b081e9ceb58a5547a21b45c38b
john2308 <gianni.elio@hotmail.it>
2014-02-02 19:01:31 +0100

    DB/SQL: Some corrections on swamp of sorrows horde sql
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/41_world_zone_swamp_of_sorrows_horde.sql
+++ sql/updates/world/41_world_zone_swamp_of_sorrows_horde.sql
@@ -117,19 +117,6 @@ INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type
 (@ENTRY,@SOURCETYPE,1,0,37,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On AI Init - Set Passive"),
 (@ENTRY,@SOURCETYPE,2,0,4,0,100,0,0,0,0,0,49,0,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Aggro - Attack Invoker"),
 (@ENTRY,@SOURCETYPE,3,0,11,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Respawn - Set Passive"),
-(@ENTRY,@SOURCETYPE,4,0,11,0,100,0,0,0,0,0,59,1,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Respawn - Set Run");
-
--- Marshtide Knight
-SET @ENTRY := 46414;
-SET @SOURCETYPE := 0;
-
-DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
-UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
-INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
-(@ENTRY,@SOURCETYPE,0,0,37,0,100,0,0,0,0,0,59,1,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On AI Init - Set Run"),
-(@ENTRY,@SOURCETYPE,1,0,37,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On AI Init - Set Passive"),
-(@ENTRY,@SOURCETYPE,2,0,4,0,100,0,0,0,0,0,49,0,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Aggro - Attack Invoker"),
-(@ENTRY,@SOURCETYPE,3,0,11,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Respawn - Set Passive"),
 (@ENTRY,@SOURCETYPE,4,0,11,0,100,0,0,0,0,0,59,1,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Respawn - Set Run"),
 (@ENTRY,@SOURCETYPE,5,0,1,0,100,0,5000,5000,5000,5000,49,0,0,0,0,0,0,11,46765,30,0,0.0,0.0,0.0,0.0,"OOC - Attack Ogre");
 
