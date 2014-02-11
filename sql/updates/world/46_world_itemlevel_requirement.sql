9975a06640f237892eb8dbe49406bd081d67a03d
ZackTsuna <Dreadkiller@gmx.de>
2014-02-11 15:52:51 +0100

    DB/Dungeons: added itemlevelrequirement to all cataclysm dungeons

---
+++ sql/updates/world/46_world_itemlevel_requirement.sql
@@ -0,0 +1,20 @@
+﻿REPLACE INTO `lfg_gear_score` (`dungeonId`, `gearScore`, `Comment`) VALUES
+-- 4.0.6 Dungeons
+(301, 329, 'Heroic Random (Cataclysm)'),
+(319, 329, 'The Vortex Pinnacle Heroic'),
+(320, 329, 'The Stonecore Heroic'),
+(321, 329, 'Halls of Origination'),
+(322, 329, 'Grim Batol Heroic'),
+(323, 329, 'Blackrock Caverns Heroic'),
+(324, 329, 'Throne of the Tides Heroic'),
+(325, 329, 'The Lost City of the Tol\'Vir Heroic'),
+(326, 329, 'Deadmines Heroic'),
+(327, 329, 'Shadowfang Keep Heroic'),
+-- 4.1.0 Dungeons
+(334, 346, 'Zul\'Gurub Heroic'),
+(340, 346, 'Zul\'Aman Heroic'),
+-- 4.3.0 Dungeons
+(434, 353, 'Heroic Random (Hour of Twlight)'),
+(435, 353, 'End Time Heroic'),
+(437, 353, 'Well of Eternity'),
+(439, 353, 'Hour of Twilight');
