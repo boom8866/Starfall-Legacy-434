ff24e36c7df6ca96d572da26f29f4d7da915cf0b
ZackTsuna <Dreadkiller@gmx.de>
2014-03-11 17:58:18 +0100

    DB: moved some game event related spawns to proper events / phases

---
+++ sql/updates/world/59_world_game_event.sql
@@ -0,0 +1,9 @@
+﻿REPLACE INTO `game_event_gameobject` (`eventEntry`, `guid`) VALUES
+(67, 46517),
+(67, 46504);
+
+-- Phasemask update for Human Commoner (id: 18927) in zone: 1519, area: 5148
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 760435;
+
+-- Phasemask update for Human Commoner (id: 18927) in zone: 1519, area: 5148
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 760436;
