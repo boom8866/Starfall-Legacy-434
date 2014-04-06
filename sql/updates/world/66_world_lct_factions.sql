e279394797acae485e6f3ec975ed9648cf0d2cca
ZackTsuna <Dreadkiller@gmx.de>
2014-04-06 13:28:10 +0200

    DB/LCT: set exp = 3 for nonhc corrected npc's

--- sql/updates/world/66_world_lct_factions.sql
+++ sql/updates/world/66_world_lct_factions.sql
@@ -1,1 +1,1 @@
-﻿UPDATE `creature_template` SET `faction_A`= 16, `faction_H`= 16 WHERE `entry` IN (43658, 43927, 45259, 45268, 45269);
+﻿UPDATE `creature_template` SET `faction_A`= 16, `faction_H`= 16, `exp`= 3 WHERE `entry` IN (43658, 43927, 45259, 45268, 45269);
