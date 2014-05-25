aa4349a2747089f947cf274421a4352fb8353ba6
john2308 <gianni.elio@hotmail.it>
2014-05-25 14:25:35 +0200

    DB/Kezan: Steamwheedle Sharks should despawn after death
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/28_world_kezan_final_adjustments.sql
+++ sql/updates/world/28_world_kezan_final_adjustments.sql
@@ -404,9 +404,9 @@ UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,8,0,100,0,69993,1,0,0,37,0,0,0,0,0,0,2,0,0,0,0.0,0.0,0.0,0.0,"On Spellhit - Die"),
 (@ENTRY,@SOURCETYPE,1,0,8,0,100,0,69993,1,0,0,33,37114,0,0,0,0,0,23,0,0,0,0.0,0.0,0.0,0.0,"On Spellhit - Quest Credit"),
-(@ENTRY,@SOURCETYPE,2,0,6,0,100,0,0,0,0,0,41,5000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Death - Despawn"),
+(@ENTRY,@SOURCETYPE,2,0,8,0,100,0,69993,1,0,0,41,3000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Death - Despawn"),
 (@ENTRY,@SOURCETYPE,3,0,54,0,100,0,0,0,0,0,69,0,0,0,0,0,0,7,0,0,0,0.0,0.0,0.0,0.0,"On Just Summoned - Move To Owner"),
-(@ENTRY,@SOURCETYPE,4,0,34,0,100,0,8,0,0,0,41,2000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Movement Inform - Force Despawn"),
+(@ENTRY,@SOURCETYPE,4,0,34,0,100,0,8,0,0,0,41,1000,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"On Movement Inform - Force Despawn"),
 (@ENTRY,@SOURCETYPE,5,0,34,0,100,0,8,0,0,0,51,0,0,0,0,0,0,11,37179,5,0,0.0,0.0,0.0,0.0,"On Movement Inform - Kill Buccaneer");
 
 DELETE FROM `spell_script_names` WHERE `spell_id` = '69987';
