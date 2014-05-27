5410022e8e287ada8943a1ca64b2f667036e4639
ZackTsuna <Dreadkiller@gmx.de>
2014-05-27 14:49:49 +0200

    DB/Testrealm: added initial sql files to enable instances, phase out inclomplete bosses and downgrade s11 vendors to s9. also phased out t12 and t13 gear vendors

---
+++ sql/updates/world/Testrealm Setup/world_instance_change.sql
@@ -0,0 +1,32 @@
+﻿-- Phasemask update for Asaad (id: 43875) in zone: 5035, area: 5035
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 778231;
+
+-- Phasemask update for Isiset (id: 39587) in zone: 4945, area: 4945
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 218703;
+
+-- Phasemask update for Ammunae (id: 39731) in zone: 4945, area: 4945
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 218344;
+
+-- Phasemask update for High Priestess Azil (id: 42333) in zone: 5088, area: 5088
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 222289;
+
+-- Phasemask update for Occu'thar (id: 52363) in zone: 5600, area: 5600
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 103;
+
+-- Phasemask update for Disciple of Hate (id: 56350) in zone: 5600, area: 5600
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 85;
+
+-- Phasemask update for Disciple of Hate (id: 56350) in zone: 5600, area: 5600
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 110;
+
+-- Phasemask update for Alizabal (id: 55869) in zone: 5600, area: 5600
+UPDATE `creature` SET `phasemask` = 2 WHERE `guid` = 112;
+
+DELETE FROM `disables` WHERE `comment` IN
+('Lost City of the Tol\'vir (4.0) - Close',
+'The Deadmines (4.0) - Close',
+'Throne of the four Winds (4.0) - Close',
+'Shadowfang Keep (4.0) - Close',
+'Stonecore (4.0) - Close',
+'Bastion of Twliight (4.0) - Close');
+
