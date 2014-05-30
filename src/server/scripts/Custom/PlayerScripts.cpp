478521fb039da504e89cf93eae65b5e27ffe808e
john2308 <gianni.elio@hotmail.it>
2014-05-30 15:29:46 +0200

    Core/Teleport: Try to fix teleport/summon stuck problems
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/scripts/Custom/PlayerScripts.cpp
+++ src/server/scripts/Custom/PlayerScripts.cpp
@@ -50,14 +50,15 @@ class TeleportUnstucker : public PlayerScript
     public:
         TeleportUnstucker() : PlayerScript("player_teleport_unstucker") { }
 
+        enum spellId
+        {
+            SPELL_LOGINEFFECT   = 836
+        };
+
         void OnMapChanged(Player* player)
         {
             player->SendMovementFlagUpdate(true);
-            x = player->GetPositionX();
-            y = player->GetPositionY();
-            z = player->GetPositionZ();
-
-            player->Relocate(x, y, z);
+            player->CastWithDelay(3000, player, SPELL_LOGINEFFECT, true);
             if (player->getClass() == CLASS_DEATH_KNIGHT)
             {
                 if (player->GetPrimaryTalentTree(player->GetActiveSpec()) == TALENT_TREE_DEATH_KNIGHT_FROST)
