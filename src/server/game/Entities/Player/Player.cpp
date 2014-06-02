0460d7f40ef49e921e5bb8d7fef7d696b4af6766
john2308 <gianni.elio@hotmail.it>
2014-06-02 18:07:04 +0200

    Core/Movements: Fixed something about charmed/possessed functions Core/CLRF: Fixed CLRF on several files
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -24785,7 +24785,7 @@ void Player::SetClientControl(Unit* target, uint8 allowMove)
     data.append(target->GetPackGUID());
     data << uint8(allowMove);
     GetSession()->SendPacket(&data);
-    if (target == this)
+    if (target == this && allowMove == 1)
         SetMover(this);
 }
 
