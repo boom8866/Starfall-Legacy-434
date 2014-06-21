94a414feff095d21e75bc9df28297200202c94c8
ZackTsuna <Dreadkiller@gmx.de>
2014-06-21 11:16:23 +0200

    DB/VP: fixed models of heroic trash mobs

---
+++ sql/updates/world/54_world_vortex_pinnacle.sql
@@ -0,0 +1,4 @@
+﻿UPDATE `creature_template` SET `modelid1`= 33828 WHERE `entry`= 45916;
+UPDATE `creature_template` SET `modelid1`= 19673 WHERE `entry`= 45923;
+UPDATE `creature_template` SET `modelid1`= 34771 WHERE `entry`= 45920;
+UPDATE `creature_template` SET `flags_extra`= 131 WHERE `entry`= 45991;
