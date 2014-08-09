d74b7eaf849e597cb990295d9d8020e10bebd199
john2308 <gianni.elio@hotmail.it>
2014-08-09 18:59:39 +0200

    DB/Vendors: Adjust some vendors to match with same content enabled
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/Testrealm Setup/world_vendor_downgrade_2nd_fix.sql
@@ -0,0 +1,29 @@
+DELETE FROM `npc_vendor` WHERE `entry` = '51256' AND `item` IN (64791, 64790, 64793);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `ExtendedCost`) VALUES
+(51256, 170, 64791, 2949),
+(51256, 169, 64790, 2949),
+(51256, 171, 64793, 2949);
+
+DELETE FROM `npc_vendor` WHERE  `entry`=51256 AND `item`=60586 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE  `entry`=51256 AND `item`=60589 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry` = '51256' AND `item` IN (64791, 64790, 64793);
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `ExtendedCost`) VALUES
+(51256, 170, 64791, 2949),
+(51256, 169, 64790, 2949),
+(51256, 171, 64793, 2949);
+
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60586 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60589 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60414 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60417 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60453 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60457 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60463 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60467 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60509 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60513 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60533 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60536 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60613 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60630 AND `ExtendedCost`=3169 AND `type`=1;
+DELETE FROM `npc_vendor` WHERE `entry`=51256 AND `item`=60636 AND `ExtendedCost`=3169 AND `type`=1;
