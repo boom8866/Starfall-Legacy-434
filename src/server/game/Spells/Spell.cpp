11028d3bfd966da96795d24c1fd2ecd0265f452d
john2308 <gianni.elio@hotmail.it>
2014-09-08 19:22:51 +0200

    Core/Pets: Add a possible crashfix in spells power type checker for pet casts
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -6073,12 +6073,17 @@ SpellCastResult Spell::CheckPetCast(Unit* target)
     // Check power type and compare with pet power amount
     Powers powerType = Powers(m_spellInfo->PowerType);
     int32 powerCost = m_spellInfo->CalcPowerCost(m_caster, m_spellInfo->GetSchoolMask());
-    if (Pet* pet = m_caster->ToPet())
+    if (m_caster)
     {
-        if (pet->GetPower(powerType) < powerCost)
-            return SPELL_FAILED_NO_POWER;
+        if (Pet* pet = m_caster->ToPet())
+        {
+            if (powerCost && powerCost > 0 && (powerType == POWER_MANA || powerType == POWER_FOCUS || powerType == POWER_ENERGY))
+            {
+                if (pet->GetPower(powerType) < powerCost)
+                    return SPELL_FAILED_NO_POWER;
+            }
+        }
     }
-
     // Cooldown
     if (Creature const* creatureCaster = m_caster->ToCreature())
     {
