6e37d1a1d0a37067f3f777dea013e35637847ff2
ZackTsuna <Dreadkiller@gmx.de>
2014-09-13 16:35:51 +0200

    DB/Gilneas: add sql file for further basic updates

---
+++ sql/updates/world/Development/Gilneas/world_gilneas_work.sql
@@ -0,0 +1,7 @@
+﻿-- Generic Updates
+UPDATE `creature_template` SET `InhabitType`= 4 WHERE `entry` IN (28332);
+
+-- Forsaken Catapults (When Mechanist dies, the catapult becomes friendly and can be used as personal catapult)
+DELETE FROM `vehicle_template_accessory` WHERE `entry`= 36283;
+INSERT INTO `vehicle_template_accessory` (`entry`, `accessory_entry`, `seat_id`, `minion`, `description`, `summontype`, `summontimer`) VALUES
+(36283, 36292, 0, 1, 'Forsaken Catapult - Forsaken Mechanist', 8, 0);
