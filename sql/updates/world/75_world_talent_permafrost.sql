a608c36239f7f85cf7bc125fa3b6c9975e68a9d8
john2308 <gianni.elio@hotmail.it>
2014-09-15 15:45:15 +0200

    DB/SQL: Permafrost heal will not take benefits from owner spellpower anymore
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/75_world_talent_permafrost.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` = '91394';
+INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES
+(91394, '0', '0', '0', '0', 'Permafrost Heal');
