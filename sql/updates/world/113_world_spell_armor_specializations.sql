ec7fa313be712b3c1c08af10f6889a0cb714fb1c
john2308 <gianni.elio@hotmail.it>
2014-10-19 12:35:54 +0200

    DB/Spells: Fixed Armor Specialization triggering
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/113_world_spell_armor_specializations.sql
@@ -0,0 +1,9 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (86530, 86531, 86529, 86528, 86525, 86524, 86526);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(86530, 'spell_gen_armor_specialization'),
+(86531, 'spell_gen_armor_specialization'),
+(86529, 'spell_gen_armor_specialization'),
+(86528, 'spell_gen_armor_specialization'),
+(86525, 'spell_gen_armor_specialization'),
+(86524, 'spell_gen_armor_specialization'),
+(86526, 'spell_gen_armor_specialization');
