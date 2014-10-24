7a2a7be4a1e165499a8e14ed08a674f658ad3ad6
john2308 <gianni.elio@hotmail.it>
2014-10-24 15:15:29 +0200

    DB/Quests: Fixed Pebble triggering after resting in place (Deepholm)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/130_world_pebble_triggering.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_area` WHERE `spell` = '80667' AND `quest_start` = '28869';
+INSERT INTO `spell_area` (`spell`, `area`, `quest_start`, `quest_end`, `aura_spell`, `racemask`, `gender`, `autocast`, `quest_start_status`, `quest_end_status`) VALUES
+(80667, 5294, 28869, 26507, 0, 0, 2, 1, 74, 11);
