4fef2f8fcf27952eaa05c607c01c54a4e1c89928
ZackTsuna <Dreadkiller@gmx.de>
2014-11-22 19:36:30 +0100

    Scripts/BoT: valiona will now select nontanks for blackout

--- src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_theralion_and_valiona.cpp
+++ src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_theralion_and_valiona.cpp
@@ -474,7 +474,7 @@ public:
                     break;
                 case EVENT_BLACKOUT:
                     Talk(SAY_VALIONA_BLACKOUT_ANNOUNCE);
-                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 0, true, 0))
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, NonTankTargetSelector(me, true)))
                         DoCast(target, SPELL_BLACKOUT_AOE);
                     events.ScheduleEvent(EVENT_BLACKOUT, 45000);
                     break;
