45022b63f3145e3bb158cf48d7aa813a1a9c0c03
john2308 <gianni.elio@hotmail.it>
2014-11-23 14:51:21 +0100

    DB/SQL: Little adjustment to an SQL file to prevent errors
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/03_world_twilight_highlands_alliance_adjustments.sql
+++ sql/updates/world/03_world_twilight_highlands_alliance_adjustments.sql
@@ -1,4 +1,4 @@
-DELETE FROM `conditions` WHERE `SourceEntry` = '27044' AND `SourceTypeOrReferenceId` = '20' AND `ConditionTypeOrReference`='8';
+fDELETE FROM `conditions` WHERE `SourceEntry` = '27044' AND `SourceTypeOrReferenceId` = '20' AND `ConditionTypeOrReference`='8';
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `ConditionTypeOrReference`, `SourceEntry`, `ConditionValue1`, `ElseGroup`) VALUES
 (20, 8, 27044, 26977, 0),
 (20, 8, 27044, 26997, 0);
@@ -9,4 +9,3 @@ INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `ConditionTypeOrReference`,
 (19, 8, 27044, 26997, 0);
 
 UPDATE `quest_template` SET `PrevQuestId`='27064' WHERE `Id`=28807;
-
