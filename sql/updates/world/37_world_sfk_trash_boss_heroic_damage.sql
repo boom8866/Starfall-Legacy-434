9fae9ea4abec13a519e7401641e097e71f4bd05d
john2308 <gianni.elio@hotmail.it>
2014-12-02 17:56:28 +0100

    DB/SFK: Updated all trash and boss damage in Shadowfang Keep (Heroic)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/37_world_sfk_trash_boss_heroic_damage.sql
@@ -0,0 +1,74 @@
+-- DamageModifier update for Frantic Geist (id: 48842)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48842;
+
+-- DamageModifier update for Corpse Eater (id: 48851)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48851;
+
+-- DamageModifier update for Baron Ashbury (id: 49708)
+UPDATE `creature_template` SET `DamageModifier`= 41 WHERE `entry`= 49708;
+
+-- DamageModifier update for Mindless Horror (id: 48919)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48919;
+
+-- DamageModifier update for Haunted Servitor (id: 48942)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48942;
+
+-- DamageModifier update for Ghostly Cook (id: 48981)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48981;
+
+-- DamageModifier update for Shadowy Attendant (id: 48973)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48973;
+
+-- DamageModifier update for Spitebone Skeleton (id: 48988)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48988;
+
+-- DamageModifier update for Wailing Guardsman (id: 48985)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 48985;
+
+-- DamageModifier update for Tormented Officer (id: 49059)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49059;
+
+-- DamageModifier update for Commander Springvale (id: 49710)
+UPDATE `creature_template` SET `DamageModifier`= 41 WHERE `entry`= 49710;
+
+-- DamageModifier update for Wailing Guardsman (id: 50614)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 50614;
+
+-- DamageModifier update for Tormented Officer (id: 50616)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 50616;
+
+-- DamageModifier update for Fetid Ghoul (id: 49126)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49126;
+
+-- DamageModifier update for Sorcerous Skeleton (id: 49130)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49130;
+
+-- DamageModifier update for Lesser Gargoyle (id: 49155)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49155;
+
+-- DamageModifier update for Stone Sleeper (id: 49160)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49160;
+
+-- DamageModifier update for Dark Creeper (id: 49125)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49125;
+
+-- DamageModifier update for Dark Creeper (id: 49125)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49125;
+
+-- DamageModifier update for Unstable Ravager (id: 49184)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49184;
+
+-- DamageModifier update for Lord Walden (id: 49711)
+UPDATE `creature_template` SET `DamageModifier`= 41 WHERE `entry`= 49711;
+
+-- DamageModifier update for Spitebone Flayer (id: 49186)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49186;
+
+-- DamageModifier update for Dread Scryer (id: 49164)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49164;
+
+-- DamageModifier update for Pustulant Monstrosity (id: 49187)
+UPDATE `creature_template` SET `DamageModifier`= 12 WHERE `entry`= 49187;
+
+-- DamageModifier update for Lord Godfrey (id: 49712)
+UPDATE `creature_template` SET `DamageModifier`= 41 WHERE `entry`= 49712;
