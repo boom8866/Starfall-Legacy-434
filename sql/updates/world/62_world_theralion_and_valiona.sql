a95aa5c2362081e21593aa06e36974da13a9d64d
Northstrider <Id0nth4ve@ny.3mail>
2014-12-31 00:27:46 +0100

    DB/BoT: add sql for recent commit

---
+++ sql/updates/world/62_world_theralion_and_valiona.sql
@@ -0,0 +1,9 @@
+REPLACE INTO `spell_script_names` (`spell_id`, `scriptname`) VALUES
+(88436, 'spell_tav_twilight_shift'),
+(92892, 'spell_tav_twilight_shift'),
+(92893, 'spell_tav_twilight_shift'),
+(92894, 'spell_tav_twilight_shift'),
+(86202, 'spell_tav_twilight_shift'),
+(92889, 'spell_tav_twilight_shift'),
+(92890, 'spell_tav_twilight_shift'),
+(92891, 'spell_tav_twilight_shift');
