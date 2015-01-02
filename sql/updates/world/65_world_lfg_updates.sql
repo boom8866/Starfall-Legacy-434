dd66b420049fc4ce164f004508308130904f6535
ZackTsuna <Dreadkiller@gmx.de>
2015-01-02 01:10:17 +0100

    DB/LFG: as in patch 4.3.0 changed: all random heroic dungeons will now grant 150 Valor Points *LFR Dungeons 'Siege of the Wyrmrest Temple' and 'The Fall of Deathwing' will now grant 250 Valor Points and have a item level requirement of 372

---
+++ sql/updates/world/65_world_lfg_updates.sql
@@ -0,0 +1,8 @@
+UPDATE `quest_template` SET `RewardCurrencyCount1`= 150 WHERE `id` IN (28905, 28906, 28907, 28908, 29185, 29183);
+UPDATE `quest_template` SET `RewardCurrencyCount1`= 250 WHERE `id` IN (30110, 30111);
+
+DELETE FROM `lfg_gear_score` WHERE `dungeonId` IN (416, 417);
+INSERT INTO `lfg_gear_score` (`dungeonId`,`gearScore`,`Comment`) VALUES
+-- 372 gearScore
+(416, 372, 'Siege of the Wyrmrest Temple'),
+(417, 372, 'Fall of Deathwing');
