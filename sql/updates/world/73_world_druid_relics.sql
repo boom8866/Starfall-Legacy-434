82236548795060233d2db9aca31b23f5813f9566
john2308 <gianni.elio@hotmail.it>
2015-01-09 16:43:45 +0100

    DB/Druid: Fixed Relic itemlevel detection for druids
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/73_world_druid_relics.sql
@@ -0,0 +1,3 @@
+DELETE FROM `playercreateinfo_spell` WHERE `classmask` = '1024' AND `Spell` = '27764';
+INSERT INTO `playercreateinfo_spell` (`classmask`, `Spell`, `Note`) VALUES
+(1024, 27764, 'Relic');
