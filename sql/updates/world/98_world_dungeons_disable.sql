3b1eb32fd234d4b20bd1fd8633f0a7520e66c81e
john2308 <gianni.elio@hotmail.it>
2015-01-23 18:12:28 +0100

    DB/Instance: Disable Deadmines and Throne of the Four Winds since are not finished yet
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/98_world_dungeons_disable.sql
@@ -0,0 +1,4 @@
+DELETE FROM `disables` WHERE `sourceType` = '2' AND `entry` IN (36, 754);
+INSERT INTO `disables` (`sourceType`, `entry`, `flags`, `params_0`, `params_1`, `comment`) VALUES
+(2, 36, 3, '0', '0', 'The Deadmines (4.0) - Close'),
+(2, 754, 14, '0', '0', 'Throne of the Four Winds (4.0) - Close');
