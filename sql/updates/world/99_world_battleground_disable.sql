a059ea76e1d439b3d9d993f33eb9ec26be224b5b
john2308 <gianni.elio@hotmail.it>
2015-01-23 19:52:15 +0100

    DB/Battlegrounds: Disable Alterac Valley, Isle of Conquest and Strand of the Ancients (need more population)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/99_world_battleground_disable.sql
@@ -0,0 +1,5 @@
+DELETE FROM `disables` WHERE `sourceType` = '3' AND `entry` IN (30, 1, 9);
+INSERT INTO `disables` (`sourceType`, `entry`, `params_0`, `params_1`, `comment`) VALUES
+(3, 30, '0', '0', 'Battleground - Isle of Conquest'),
+(3, 1, '0', '0', 'Battleground - Alterac Valley'),
+(3, 9, '0', '0', 'Battleground - Strand of the Ancients');
