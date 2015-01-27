db03761fd1387c3cb9fff7d7723b72a76902037c
ZackTsuna <Dreadkiller@gmx.de>
2015-01-28 00:04:11 +0100

    Scripts/BH: added savestream data for baradin hold instance script

--- src/server/scripts/EasternKingdoms/BaradinHold/instance_baradin_hold.cpp
+++ src/server/scripts/EasternKingdoms/BaradinHold/instance_baradin_hold.cpp
@@ -113,6 +113,60 @@ public:
             }
         }
 
+        bool SetBossState(uint32 data, EncounterState state)
+        {
+            if (!InstanceScript::SetBossState(data, state))
+                return false;
+
+            return true;
+        }
+
+        std::string GetSaveData()
+        {
+            OUT_SAVE_INST_DATA;
+
+            std::ostringstream saveStream;
+            saveStream << "B H " << GetBossSaveData();
+
+            OUT_SAVE_INST_DATA_COMPLETE;
+            return saveStream.str();
+        }
+
+        void Load(const char* str)
+        {
+            if (!str)
+            {
+                OUT_LOAD_INST_DATA_FAIL;
+                return;
+            }
+
+            OUT_LOAD_INST_DATA(str);
+
+            char dataHead1, dataHead2;
+
+            std::istringstream loadStream(str);
+            loadStream >> dataHead1 >> dataHead2;
+
+            if (dataHead1 == 'B' && dataHead2 == 'H')
+            {
+                for (uint32 i = 0; i < MAX_ENCOUNTERS; ++i)
+                {
+                    uint32 tmpState;
+                    loadStream >> tmpState;
+                    if (tmpState == IN_PROGRESS || tmpState > SPECIAL)
+                        tmpState = NOT_STARTED;
+                    SetBossState(i, EncounterState(tmpState));
+                }
+
+                uint32 temp = 0;
+                loadStream >> temp;
+            }
+            else
+                OUT_LOAD_INST_DATA_FAIL;
+
+            OUT_LOAD_INST_DATA_COMPLETE;
+        }
+
     private:
         uint64 ArgalothGUID;
         uint64 OccutharGUID;
