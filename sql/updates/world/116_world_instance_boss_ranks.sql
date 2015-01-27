0b9c124292401013f99d2b204fba5d52634439ea
john2308 <gianni.elio@hotmail.it>
2015-01-27 13:46:50 +0100

    DB/Instances: Correct all ranks for bosses in Throne of the Tides and Blackrock Caverns
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/116_world_instance_boss_ranks.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `rank`=3 WHERE `entry` IN (40825, 49072);
+
+UPDATE `creature_template` SET `rank`=3 WHERE  `entry` IN 
+(39679, 39665, 39698, 39700, 39705, 39666, 39680, 39699, 39701, 39706); 
