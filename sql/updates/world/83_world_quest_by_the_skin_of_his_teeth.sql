251a8c2a2d5edf4c0b71066ebeb7baf8d77a8cbe
ZackTsuna <Dreadkiller@gmx.de>
2015-02-02 22:44:44 +0100

    Because project related <3

--- sql/updates/world/83_world_quest_by_the_skin_of_his_teeth.sql
+++ sql/updates/world/83_world_quest_by_the_skin_of_his_teeth.sql
@@ -1,2 +1,2 @@
-UPDATE `creature_template` SET `BaseAttackTime`=0, `RangeAttackTime`=0 WHERE `entry`=35077;
+UPDATE `creature_template` SET `BaseAttackTime`=2000, `RangeAttackTime`=0 WHERE `entry`=35077;
 UPDATE `creature_template` SET `DamageModifier`=0.4 WHERE `entry` IN (35456, 35167);
