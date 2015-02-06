863a59de7e8b25efef0392ed254df60e2a557854
john2308 <gianni.elio@hotmail.it>
2015-02-06 22:57:12 +0100

    DB/HoO: Added spells to Living Vine
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/146_world_hoo_boss_trash_damage_normal.sql
+++ sql/updates/world/146_world_hoo_boss_trash_damage_normal.sql
@@ -61,3 +61,15 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCE
 UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
 (@ENTRY,@SOURCETYPE,0,0,37,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"AI Init - Set Passive");
+
+-- Living Vine
+SET @ENTRY := 40668;
+SET @SOURCETYPE := 0;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=@SOURCETYPE;
+UPDATE creature_template SET AIName="SmartAI" WHERE entry=@ENTRY LIMIT 1;
+INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES 
+(@ENTRY,@SOURCETYPE,0,0,0,0,100,0,3000,7000,10000,10000,11,75153,0,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"IC - Cast Spore Blast"),
+(@ENTRY,@SOURCETYPE,1,0,1,0,100,0,3000,7000,10000,10000,11,75153,0,0,0,0,0,5,0,0,0,0.0,0.0,0.0,0.0,"OOC - Cast Spore Blast"),
+(@ENTRY,@SOURCETYPE,2,0,37,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"AI Init - Set Passive"),
+(@ENTRY,@SOURCETYPE,3,0,37,0,100,0,0,0,0,0,18,4,0,0,0,0,0,1,0,0,0,0.0,0.0,0.0,0.0,"AI Init - Set Rooted");
