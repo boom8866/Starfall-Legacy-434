66a5f5fdd6ad6179cc49b400c9354d95fe92fa21
john2308 <gianni.elio@hotmail.it>
2015-02-13 21:19:29 +0100

    Core/HoO: Finished work on trash groups before Ptah's stairs
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/160_world_hoo_earthrager_ptah_stairs.sql
@@ -0,0 +1,20 @@
+DELETE FROM `creature_formations` WHERE `leaderGUID` = '218644';
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(218644, 218644, 0, 0, 2),
+(218644, 218645, 0, 0, 2),
+(218644, 218646, 0, 0, 2);
+
+DELETE FROM `creature_formations` WHERE `leaderGUID` = '218648';
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(218648, 218647, 0, 0, 2),
+(218648, 218648, 0, 0, 2),
+(218648, 218651, 0, 0, 2),
+(218648, 218652, 0, 0, 2),
+(218648, 218650, 0, 0, 2);
+
+DELETE FROM `creature_formations` WHERE `leaderGUID` = '218656';
+INSERT INTO `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) VALUES
+(218656, 218656, 0, 0, 2),
+(218656, 218655, 0, 0, 2),
+(218656, 218653, 0, 0, 2),
+(218656, 218654, 0, 0, 2);
