13c1a0735fa8624037b71443190d5b4d2a1ff3d1
john2308 <gianni.elio@hotmail.it>
2015-02-15 14:20:54 +0100

    Core/Glyphs: Glyph of Lava Lash will not be applied twice anymore
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5196,10 +5196,6 @@ void Spell::EffectWeaponDmg (SpellEffIndex effIndex)
                     if (m_caster->GetAuraEffect(SPELL_AURA_DUMMY, SPELLFAMILY_SHAMAN, 0x200000, 0, 0))
                         totalDamagePercentMod += totalDamagePercentMod * 0.40f;
 
-                    // Glyph of Lava Lash
-                    if (m_caster->HasAura(55444))
-                        totalDamagePercentMod += totalDamagePercentMod * 0.20f;
-
                     // Each points of Mastery increases damage by an additional 2.5%
                     if (m_caster->GetTypeId() == TYPEID_PLAYER)
                     {
