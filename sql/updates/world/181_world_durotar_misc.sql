782e8f6c04e495c596d71db9761e9142a9b22458
Northstrider <Id0nth4ve@ny.3mail>
2015-02-20 00:59:32 +0100

    DB/Durotar: removed questgiver flag from northwatch scout and mottled boar *added copper drops to northwatch scout

---
+++ sql/updates/world/181_world_durotar_misc.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `unit_flags`= 0, `npcflag`= 0 WHERE `entry` IN (3098, 39317);
+-- Northwatch Scout
+UPDATE `creature_template` SET `mingold`= 2, `maxgold`= 4 WHERE `entry`= 39317;
