a35078cd8f8383892ee501ea994194469f80ec5c
john2308 <gianni.elio@hotmail.it>
2015-02-25 20:59:32 +0100

    Core/DB/Quests: Some little adjustments in Twilight Highlands
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/scripts/EasternKingdoms/zone_twilight_highlands.cpp
+++ src/server/scripts/EasternKingdoms/zone_twilight_highlands.cpp
@@ -2912,7 +2912,7 @@ public:
                         RESCHEDULE_IF_CASTING;
                         if (Unit* victim = me->getVictim())
                         {
-                            if (me->GetHealth() <= me->GetMaxHealth() * 0.50f)
+                            if (me->GetHealth() >= me->GetMaxHealth() * 0.75f)
                                 DoCast(victim, SPELL_STATIC_FLUX);
                         }
                         events.RescheduleEvent(EVENT_STATIC_FLUX, 6000);
@@ -15939,6 +15939,8 @@ public:
             me->GetMotionMaster()->MovePoint(POINT_CENTER, -4182.39f, -5148.45f, -7.73f, false);
             me->SetReactState(REACT_AGGRESSIVE);
             events.ScheduleEvent(EVENT_CHECK_EVADE, 20000);
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 8 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 8 + me->getLevel())));
         }
 
         void JustDied(Unit* /*killer*/)
@@ -16105,6 +16107,8 @@ public:
             TalkWithDelay(3000, 0);
             me->SetReactState(REACT_AGGRESSIVE);
             events.ScheduleEvent(EVENT_CHECK_EVADE, 20000);
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 9 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 9 + me->getLevel())));
         }
 
         void JustDied(Unit* /*killer*/)
@@ -16283,6 +16287,8 @@ public:
             TalkWithDelay(3000, 0);
             me->SetReactState(REACT_AGGRESSIVE);
             events.ScheduleEvent(EVENT_CHECK_EVADE, 20000);
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 9 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 9 + me->getLevel())));
         }
 
         void JustDied(Unit* /*killer*/)
@@ -16477,6 +16483,8 @@ public:
             events.ScheduleEvent(EVENT_ENTER_COMBAT, 17000);
             me->SetReactState(REACT_AGGRESSIVE);
             events.ScheduleEvent(EVENT_CHECK_EVADE, 20000);
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 10 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 10 + me->getLevel())));
         }
 
         void EnterEvadeMode()
@@ -16833,6 +16841,8 @@ public:
         {
             eventCarriageDone = false;
             me->SetHealth(me->GetMaxHealth());
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 10 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 10 + me->getLevel())));
         }
 
         void JustDied(Unit* /*killer*/)
@@ -17203,6 +17213,8 @@ public:
             me->SetReactState(REACT_AGGRESSIVE);
             events.ScheduleEvent(EVENT_ENGAGE, 17000);
             events.ScheduleEvent(EVENT_CHECK_EVADE, 30000);
+            me->SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float((me->getLevel() * 12 - me->getLevel())));
+            me->SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float((me->getLevel() * 12 + me->getLevel())));
         }
 
         void JustDied(Unit* /*killer*/)
