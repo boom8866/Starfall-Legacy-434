8444898925d1969675bdab93501237fded5953c3
ZackTsuna <Dreadkiller@gmx.de>
2015-02-25 17:09:08 +0100

    Core/LFG: refix the first daily reward for classic dungeons

--- sql/updates/world/16_world_lfg_quests.sql
+++ sql/updates/world/16_world_lfg_quests.sql
@@ -1,1 +1,5 @@
-﻿UPDATE `quest_template` SET `SpecialFlags`= 9 WHERE `SpecialFlags`= 8;
+﻿UPDATE `quest_template` SET `SpecialFlags`= 9 WHERE `id` IN (SELECT `otherQuestId` FROM `lfg_dungeon_rewards`);
+
+UPDATE `quest_template` SET `SpecialFlags`= 8 WHERE `id` IN (SELECT `firstQuestId` FROM `lfg_dungeon_rewards`);
+
+UPDATE `quest_template` SET `SpecialFlags`= 9 WHERE `id` IN (25485, 25483, 25482, 25484, 28905, 29185, 30110);
