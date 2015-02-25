b5150cf1b1f6a5cbe667fb7854e1664113c82bf9
john2308 <gianni.elio@hotmail.it>
2015-02-25 19:23:19 +0100

    Core/Mastery: Final fix for Echo of Light mastery (IN TESTING)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -1368,25 +1368,30 @@ public:
 
         void HandleProc(ProcEventInfo& info)
         {
-            Unit* target = GetTarget();
+            Unit* target = info.GetActionTarget();
             Unit* caster = GetCaster();
             if (!caster || !target || !info.GetHealInfo()->GetHeal())
                 return;
 
             int32 bp0 = info.GetHealInfo()->GetHeal() * (GetEffect(EFFECT_0)->GetAmount() / 100.f);
-            SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT);
-            bp0 /= spellInfo->GetDuration() / spellInfo->Effects[EFFECT_0].Amplitude;
-            if (AuraEffect* eol = target->GetAuraEffect(SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, EFFECT_0, caster->GetGUID()))
+            if (SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT))
             {
-                uint32 tickRemaining = target->GetRemainingPeriodicAmount(caster->GetGUID(), SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, SPELL_AURA_PERIODIC_HEAL);
-                bp0 += target->GetAura(SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, caster->GetGUID())->GetEffect(EFFECT_0)->GetAmount();
-                bp0 += tickRemaining;
-
-                target->CastCustomSpell(info.GetActionTarget(), SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, &bp0, NULL, NULL, true);
-                return;
+                bp0 /= spellInfo->GetDuration() / spellInfo->Effects[EFFECT_0].Amplitude;
+                if (AuraEffect* aurEff = target->GetAuraEffect(SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, EFFECT_0, caster->GetGUID()))
+                {
+                    uint32 remainingAmount = aurEff->GetAmount();
+                    uint32 tickNumber = aurEff->GetTickNumber();
+                    if (tickNumber > 0 && remainingAmount > 0)
+                    {
+                        remainingAmount /= tickNumber;
+                        aurEff->SetAmount(aurEff->GetAmount() + remainingAmount);
+                        aurEff->GetBase()->RefreshDuration();
+                    }
+                    return;
+                }
+                else
+                    caster->CastCustomSpell(target, SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, &bp0, NULL, NULL, true);
             }
-
-            target->CastCustomSpell(info.GetActionTarget(), SPELL_PRIEST_ECHO_OF_LIGHT_EFFECT, &bp0, NULL, NULL, true);
         }
 
         void Register()
