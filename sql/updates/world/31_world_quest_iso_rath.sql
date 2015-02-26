c4633e9593127fcc62d5cb13224051f7a99b7045
john2308 <gianni.elio@hotmail.it>
2015-02-26 22:02:49 +0100

    DB/Creatures: Some little adjustments to iso'rath tentacles and the brain
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/31_world_quest_iso_rath.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `HealthModifier`=12 WHERE `entry`=47960;
+UPDATE `creature_template` SET `HealthModifier`=3 WHERE `entry`=48739;
+UPDATE `creature_template` SET `HealthModifier`=3 WHERE `entry`=48796;
