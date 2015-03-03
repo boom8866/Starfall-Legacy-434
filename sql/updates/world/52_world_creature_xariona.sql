7346f64ac77c7f2091b59616dfde87447afdbce6
john2308 <gianni.elio@hotmail.it>
2015-03-03 13:05:27 +0100

    DB/Creatures: Fixed some batching errors in Xariona sql file
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/52_world_creature_xariona.sql
+++ sql/updates/world/52_world_creature_xariona.sql
@@ -1,3 +1,3 @@
-UPDATE `creature` SET `spawntimesecs`=302400 WHERE `guid`=746683;
--- DamageModifier update for Xariona (id: 50061)
+UPDATE `creature` SET `spawntimesecs` = 302400 WHERE `guid`=746683;
 UPDATE `creature_template` SET `DamageModifier`= 300 WHERE `entry`= 50061;
+UPDATE `creature_template` SET `mechanic_immune_mask`=617299967 WHERE  `entry`=50061;
