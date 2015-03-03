fd60257b1a3c2000fc46d37fd0201217a666514c
john2308 <gianni.elio@hotmail.it>
2015-03-03 13:00:38 +0100

    DB/Creatures: Some adjustments to Xariona
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/52_world_creature_xariona.sql
@@ -0,0 +1,3 @@
+UPDATE `creature` SET `spawntimesecs`=302400 WHERE `guid`=746683;
+-- DamageModifier update for Xariona (id: 50061)
+UPDATE `creature_template` SET `DamageModifier`= 300 WHERE `entry`= 50061;
