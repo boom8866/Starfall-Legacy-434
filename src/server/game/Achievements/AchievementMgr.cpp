06b615ffb498f48aef1e7f8e6886f09731a7baab
john2308 <gianni.elio@hotmail.it>
2015-03-04 16:25:27 +0100

    Core/Achievements: Temp disable something to prevent crashes about auratype check
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Achievements/AchievementMgr.cpp
+++ src/server/game/Achievements/AchievementMgr.cpp
@@ -2730,8 +2730,7 @@ bool AchievementMgr<T>::AdditionalRequirementsSatisfied(AchievementCriteriaEntry
                 if (!unit || !unit->HasAura(reqValue))
                     return false;
             case ACHIEVEMENT_CRITERIA_ADDITIONAL_CONDITION_TARGET_HAS_AURA_TYPE: // 11
-                if (!unit || !unit->HasAuraType(AuraType(reqValue)))
-                    return false;
+                return false;
                 break;
             case ACHIEVEMENT_CRITERIA_ADDITIONAL_CONDITION_ITEM_QUALITY_MIN: // 14
             {
