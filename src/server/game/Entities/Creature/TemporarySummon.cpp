ed316356b72f55c1641b12e966bc0dba03fcf94a
john2308 <gianni.elio@hotmail.it>
2015-03-05 22:15:03 +0100

    Core/Creatures: Tempsummon owner detection system optimized (much more safe and cleany) Core/Quests: Some adjustments on several creatures scripted for quests (Addtreath function more safe) DB/Quests: Fixed a logic error for quest "Nightmare" in Twilight Highlands, now will works as intended
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Entities/Creature/TemporarySummon.cpp
+++ src/server/game/Entities/Creature/TemporarySummon.cpp
@@ -41,6 +41,29 @@ void TempSummon::Update(uint32 diff)
 {
     Creature::Update(diff);
 
+    // This check will search for summoner on every update, if the summoner is too much distant or
+    // not in world anymore, it will be instantly unsummoned!
+    Unit* summoner = GetSummoner();
+    if (summoner)
+    {
+        if (summoner->GetTypeId() == TYPEID_PLAYER)
+        {
+            if (!summoner->IsWithinDist(this, 300.0f))
+            {
+                UnSummon();
+                return;
+            }
+        }
+    }
+    else
+    {
+        if (IsInWorld())
+        {
+            UnSummon();
+            return;
+        }
+    }
+
     if (m_deathState == DEAD)
     {
         UnSummon();
