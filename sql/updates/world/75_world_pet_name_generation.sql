3b136c7416863c4a3ed907625fac72965981eb6b
john2308 <gianni.elio@hotmail.it>
2015-03-07 20:00:36 +0100

    DB/Pets: Adjust some pet name generations DB/GameObjects: Adjust Kaja'Mine respawn time
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/75_world_pet_name_generation.sql
@@ -0,0 +1,6 @@
+UPDATE `pet_name_generation` SET `word`='Grakk' WHERE `id`=138;
+UPDATE `pet_name_generation` SET `word`='Grazz' WHERE `id`=139;
+UPDATE `pet_name_generation` SET `word`='Jukk' WHERE `id`=147;
+UPDATE `pet_name_generation` SET `word`='Kall' WHERE `id`=148;
+UPDATE `pet_name_generation` SET `word`='Zagg' WHERE `id`=163;
+UPDATE `pet_name_generation` SET `word`='Zharr' WHERE `id`=165;
