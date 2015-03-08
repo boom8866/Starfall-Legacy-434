d247a1f03bbf29ee5c7549d52a7ea5482647a98c
john2308 <gianni.elio@hotmail.it>
2015-03-08 19:57:56 +0100

    DB/StackSystem: Fixed scroll of stamina stacking
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/78_world_stamina_stack.sql
@@ -0,0 +1,11 @@
+DELETE FROM `spell_group` WHERE `id` = '5005' AND `spell_id` IN (8099);
+INSERT INTO `spell_group` (`id`, `spell_id`) VALUES
+(5005, 8099),
+(5005, 8100),
+(5005, 8101),
+(5005, 12178),
+(5005, 33081),
+(5005, 43198),
+(5005, 48101),
+(5005, 48102),
+(5005, 89345);
