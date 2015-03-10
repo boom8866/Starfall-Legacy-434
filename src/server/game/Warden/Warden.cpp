4e737c2958dab4e6505710e5e60045545f4615c4
john2308 <gianni.elio@hotmail.it>
2015-03-10 14:07:16 +0100

    Core/Stability: Add some crashfixes
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Warden/Warden.cpp
+++ src/server/game/Warden/Warden.cpp
@@ -219,6 +219,9 @@ std::string Warden::Penalty(WardenCheck* check /*= NULL*/)
 
 void WorldSession::HandleWardenDataOpcode(WorldPacket& recvData)
 {
+    if (!_warden)
+        return;
+
     _warden->DecryptData(recvData.contents(), recvData.size());
     uint8 opcode;
     recvData >> opcode;
