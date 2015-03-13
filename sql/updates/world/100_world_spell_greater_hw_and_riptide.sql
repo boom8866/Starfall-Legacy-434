e20aaf7272d92fe2a74b426b26ffc15fc38e3fd9
ZackTsuna <Dreadkiller@gmx.de>
2015-03-13 16:02:27 +0100

    DB/Spells: corrected several wrong coefficients of the last commits

--- sql/updates/world/100_world_spell_greater_hw_and_riptide.sql
+++ sql/updates/world/100_world_spell_greater_hw_and_riptide.sql
@@ -1,2 +1,2 @@
-UPDATE `spell_bonus_data` SET `direct_bonus`=0.806 WHERE `entry`=77472;
-UPDATE `spell_bonus_data` SET `direct_bonus`=0.234, `dot_bonus`=0.075 WHERE `entry`=61295;
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.6999 WHERE `entry`=77472;
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.238, `dot_bonus`=0.1125 WHERE `entry`=61295;
--- sql/updates/world/99_world_spell_holy_radiance.sql
+++ sql/updates/world/99_world_spell_holy_radiance.sql
@@ -1,1 +1,1 @@
-UPDATE `spell_bonus_data` SET `direct_bonus`=0.403 WHERE `entry`=82327;
+UPDATE `spell_bonus_data` SET `direct_bonus`=0.259 WHERE `entry`=82327;
