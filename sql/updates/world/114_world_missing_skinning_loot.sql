9ecf7ceefac2f37ebb19e13ab91f9c47d8ae217c
john2308 <gianni.elio@hotmail.it>
2015-03-16 00:06:48 +0100

    DB/Professions: Missing fix in last commit
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/114_world_missing_skinning_loot.sql
+++ sql/updates/world/114_world_missing_skinning_loot.sql
@@ -1,2 +1,3 @@
 UPDATE `creature_template` SET `skinloot`=70200 WHERE `entry`=24979;
 UPDATE `skinning_loot_template` SET `ChanceOrQuestChance`=90 WHERE `entry`=70200 AND `item`=33567;
+UPDATE `skinning_loot_template` SET `ChanceOrQuestChance`=99 WHERE `entry`=70211 AND `item`=33568;
