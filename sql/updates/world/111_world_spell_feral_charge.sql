f437b9ed07dc444fb097ac2db951344bef635568
john2308 <gianni.elio@hotmail.it>
2015-03-15 17:17:30 +0100

    Core/Spells: Feral Charge will not be casted anymore while rooted Core/Mounts: Swift Magic Broom will be blocked in Battleground/Arenas
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/111_world_spell_feral_charge.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (49376, 16979);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(49376, 'spell_dru_feral_charge_cat'),
+(16979, 'spell_dru_feral_charge_bear');
