3fd03e68cb039127f6fcb788d3fada543a8e2808
john2308 <gianni.elio@hotmail.it>
2015-03-16 00:57:29 +0100

    DB/Professions: Other fixes for skinning DB/Spells: Tazik Shocker damage will not scale with spellpower anymore

---
+++ sql/updates/world/115_world_skinning_final.sql
@@ -0,0 +1,2 @@
+UPDATE `skinning_loot_template` SET `mincountOrRef`=2, `maxcount`=6 WHERE `entry`=70200 AND `item`=33568;
+UPDATE `skinning_loot_template` SET `maxcount`=3 WHERE `entry`=70211 AND `item`=33568;
---
+++ sql/updates/world/116_world_engineering_tazik_shocker.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_bonus_data` WHERE `entry` = '82179';
+INSERT INTO `spell_bonus_data` (`entry`, `comments`) VALUES
+(82179, 'Engineering - Tazik Shocker');
