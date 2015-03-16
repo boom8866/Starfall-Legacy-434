80b960780cabadc355ee4a597e1a2a6953147980
john2308 <gianni.elio@hotmail.it>
2015-03-16 17:22:46 +0100

    Core/Talents: Demonic Pact will be correctly removed if owner lost the talent DB/Spells: Add missing part for Rapid Recuperation
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- sql/updates/world/117_world_spell_hunter_rapid_recuperation.sql
+++ sql/updates/world/117_world_spell_hunter_rapid_recuperation.sql
@@ -5,3 +5,7 @@ DELETE FROM `spell_script_names` WHERE `spell_id` IN (35098, 35099);
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (35098, 'spell_hun_rapid_killing'),
 (35099, 'spell_hun_rapid_killing');
+
+DELETE FROM `spell_script_names` WHERE `spell_id` = '3045';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(3045, 'spell_hun_rapid_fire');
