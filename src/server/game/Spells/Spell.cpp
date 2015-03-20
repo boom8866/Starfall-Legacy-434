ff8a455d01c85b50e044b98c51b9cac249e6eda3
john2308 <gianni.elio@hotmail.it>
2015-03-20 14:24:56 +0100

    Core/DB/Quests: Fixed some problems with quest "Rocket Rescue" (Tanaris)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -7311,7 +7311,7 @@ bool Spell::CheckEffectTarget(Unit const* target, uint32 eff) const
     // Line of Sight check for AoE ground-pointing spells (Only for PvP for now to prevent problems in PvE)
     if (m_caster->GetTypeId() == TYPEID_PLAYER)
     {
-        if (m_spellInfo->Targets & TARGET_FLAG_DEST_LOCATION)
+        if (m_spellInfo->Targets & TARGET_FLAG_DEST_LOCATION && !(m_spellInfo->AttributesEx2 & SPELL_ATTR2_CAN_TARGET_NOT_IN_LOS))
         {
             if (m_targets.GetDstPos() && !target->IsWithinLOS(m_targets.GetDstPos()->GetPositionX(), m_targets.GetDstPos()->GetPositionY(), m_targets.GetDstPos()->GetPositionZ()))
                 return false;
