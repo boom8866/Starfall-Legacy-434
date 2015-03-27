672541d8609ee301a9e341a8e2c113aea0526231
john2308 <gianni.elio@hotmail.it>
2015-03-27 23:44:02 +0100

    Core/Spells: Fixed Blood Boil damage bonus on disease Core/Talents: Will of the Necropolis will correctly grant the free Rune Tap buff Core/Creatures: Test modify for rooted effects on creatures Core/Phasings: Add some adjustments to old quest phase updater to prevent problems with phasings (NFY) DB/Creatures: Accursed Shipbuilders in Tol'Barad Peninsula will not spam their Curse anymore
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -1296,11 +1296,9 @@ void Spell::EffectSchoolDMG (SpellEffIndex effIndex)
                 // Blood Boil - bonus for diseased targets
                 if (m_spellInfo->SpellFamilyFlags[0] & 0x00040000)
                 {
+                    damage += CalculatePct(m_caster->GetTotalAttackPowerValue(BASE_ATTACK), 6);
                     if (unitTarget->GetAuraEffect(SPELL_AURA_PERIODIC_DAMAGE, SPELLFAMILY_DEATHKNIGHT, 0, 0, 0x00000002, m_caster->GetGUID()))
-                    {
-                        damage += m_damage / 2;
-                        damage += int32(m_caster->GetTotalAttackPowerValue(BASE_ATTACK) * 0.035f);
-                    }
+                        AddPct(damage, 50);
                 }
                 break;
             }
