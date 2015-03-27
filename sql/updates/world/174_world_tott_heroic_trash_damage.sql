e72064c4585ddf1ec667c89318c7a68bc493d1a0
john2308 <gianni.elio@hotmail.it>
2015-03-27 13:46:23 +0100

    DB/ToTT: Updated melee damage of all Heroic Mode trash creatures in Throne of the Tides
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/174_world_tott_heroic_trash_damage.sql
@@ -0,0 +1,44 @@
+-- DamageModifier update for Naz'jar Invader (id: 49085)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49085;
+
+-- DamageModifier update for Naz'jar Spiritmender (id: 49090)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49090;
+
+-- DamageModifier update for Naz'jar Sentinel (id: 49087)
+UPDATE `creature_template` SET `DamageModifier`= 22 WHERE `entry`= 49087;
+
+-- DamageModifier update for Naz'jar Invader (id: 49086)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49086;
+
+-- DamageModifier update for Naz'jar Tempest Witch (id: 49092)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49092;
+
+-- DamageModifier update for Deep Murloc Hunter (id: 49067)
+UPDATE `creature_template` SET `DamageModifier`= 13 WHERE `entry`= 49067;
+
+-- DamageModifier update for Deep Murloc Hunter (id: 49067)
+UPDATE `creature_template` SET `DamageModifier`= 10 WHERE `entry`= 49067;
+
+-- DamageModifier update for Faceless Watcher (id: 49074)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49074;
+
+-- DamageModifier update for Tainted Sentry (id: 49102)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49102;
+
+-- DamageModifier update for Deep Murloc Invader (id: 49068)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49068;
+
+-- DamageModifier update for Vicious Mindlasher (id: 49107)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49107;
+
+-- DamageModifier update for Unyielding Behemoth (id: 49104)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49104;
+
+-- DamageModifier update for Faceless Sapper (id: 49073)
+UPDATE `creature_template` SET `DamageModifier`= 15 WHERE `entry`= 49073;
+
+-- DamageModifier update for Blight Beast (id: 49061)
+UPDATE `creature_template` SET `DamageModifier`= 14 WHERE `entry`= 49061;
+
+-- DamageModifier update for Naz'jar Honor Guard (id: 49084)
+UPDATE `creature_template` SET `DamageModifier`= 18 WHERE `entry`= 49084;
