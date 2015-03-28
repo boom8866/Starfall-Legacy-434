aa5cfb25306e53852b2f3c109368b518925c48eb
john2308 <gianni.elio@hotmail.it>
2015-03-28 17:27:43 +0100

    Core/Mechanics: Revert some bad fixes to prevend bugs on freeze effects
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -17744,13 +17744,19 @@ void Unit::SetStunned(bool apply)
         SetTarget(0);
         SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_STUNNED);
 
+        // MOVEMENTFLAG_ROOT cannot be used in conjunction with MOVEMENTFLAG_MASK_MOVING (tested 3.3.5a)
+        // this will freeze clients. That's why we remove MOVEMENTFLAG_MASK_MOVING before
+        // setting MOVEMENTFLAG_ROOT
+        RemoveUnitMovementFlag(MOVEMENTFLAG_MASK_MOVING);
+        AddUnitMovementFlag(MOVEMENTFLAG_ROOT);
+
         // Creature specific
         if (GetTypeId() != TYPEID_PLAYER)
             ToCreature()->StopMoving();
         else
             SetStandState(UNIT_STAND_STATE_STAND);
 
-        SetRooted(true);
+        SendMoveRoot(0);
 
         CastStop();
     }
@@ -17765,7 +17771,10 @@ void Unit::SetStunned(bool apply)
             RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_STUNNED);
 
         if (!HasUnitState(UNIT_STATE_ROOT))         // prevent moving if it also has root effect
-            SetRooted(false);
+        {
+            SendMoveUnroot(0);
+            RemoveUnitMovementFlag(MOVEMENTFLAG_ROOT);
+        }
     }
 }
 
