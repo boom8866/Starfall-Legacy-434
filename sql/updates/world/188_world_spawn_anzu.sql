326a4356a2073d5ab39ffe4996fde9d36fa3ced9
john2308 <gianni.elio@hotmail.it>
2015-03-31 18:42:18 +0200

    Core/Spells: Remove a wrong triggered spell that was causing error in console DB/DarkmoonFaire: Adjust some deck quests as repeatable quests DB/Creatures: Anzu is now already spawned since cataclysm
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/188_world_spawn_anzu.sql
@@ -0,0 +1,3 @@
+DELETE FROM `creature` WHERE `guid` = '700963';
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+(700963, 23035, 556, 2, 1, 21492, 0, -89.2041, 287.974, 26.5665, 3.00197, 86400, 0, 0, 230000, 33090, 0, 0, 0, 0);
