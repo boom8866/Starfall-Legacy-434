9f6cc9faed2fa2d616a9b780bd3b275caab76bd1
ZackTsuna <Dreadkiller@gmx.de>
2015-04-13 19:40:40 +0200

    DB/LFG: fixed a typo

--- sql/updates/world/218_world_lfg_fixups.sql
+++ sql/updates/world/218_world_lfg_fixups.sql
@@ -1,2 +1,2 @@
-﻿UPDATE `quest_template` SET `RewardCurrencyCount1`= 150, `RewardCurrencyId1`= 396 WHERE `id` IN (28906, 29183);
+﻿UPDATE `quest_template` SET `RewardCurrencyCount1`= 150, `RewardCurrencyId1`= 395 WHERE `id` IN (28906, 29183);
 UPDATE `quest_template` SET `SpecialFlags`= 9 WHERE `SpecialFlags`= 8;
