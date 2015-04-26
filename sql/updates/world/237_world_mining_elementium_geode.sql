9cb9771c3bc26561f0679138ba5fdc62eccbb0f7
john2308 <gianni.elio@hotmail.it>
2015-04-26 14:37:54 +0200

    DB/Professions: Add missing Elementium Geode from mining loots
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/237_world_mining_elementium_geode.sql
@@ -0,0 +1,4 @@
+DELETE FROM `gameobject_loot_template` WHERE `item` = '67282';
+INSERT INTO `gameobject_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`) VALUES
+(202741, 67282, 0.05, 1, 0, 1, 1),
+(202738, 67282, 0.05, 1, 0, 1, 1);
