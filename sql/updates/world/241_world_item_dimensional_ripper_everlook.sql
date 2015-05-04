ab2398023311d496ef8e96f13a60b93aed6811f5
john2308 <gianni.elio@hotmail.it>
2015-05-04 22:14:56 +0200

    Core/Spells: Fixed Unstable Affliction dispel double damage Core/Talents: Fixed Chill of the Grave internal cooldown DB/Items: Fixed Dimensional Ripper - Everlook
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/241_world_item_dimensional_ripper_everlook.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE `id` = '23442';
+INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+(23442, 1, 6706.33, -4635.52, 721.69, 1);
