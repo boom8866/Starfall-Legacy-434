3144b16c77ecb71cab831a41f0e9f08fb2b261f5
john2308 <gianni.elio@hotmail.it>
2015-05-10 21:42:30 +0200

    DB/SFK: Fix some mistakes with Lord Walden loot in normal mode DB/GB: Revert Faceless Corruptors speed
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/258_world_sfk_lord_walden_loot_normal.sql
@@ -0,0 +1,5 @@
+DELETE FROM `creature_loot_template` WHERE `entry`=46963 AND `item`=63450;
+DELETE FROM `creature_loot_template` WHERE `entry`=46963 AND `item`=63452;
+DELETE FROM `creature_loot_template` WHERE `entry`=46963 AND `item`=63453;
+DELETE FROM `creature_loot_template` WHERE `entry`=46963 AND `item`=63454;
+DELETE FROM `creature_loot_template` WHERE `entry`=46963 AND `item`=63455;
