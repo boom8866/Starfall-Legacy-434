992a0d92109559a39e14842054fc62e35b734018
john2308 <gianni.elio@hotmail.it>
2015-05-23 18:43:50 +0200

    DB/Loots: Fixed Mr. Grubbs loot from item DB/Items: Fixed Hellscream Reach tabard teleport effect
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/279_world_hellscream_reach_tabard.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_target_position` WHERE `id` = '89598';
+INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES
+(89598, 732, -603.51, 1387.72, 21.53, 0.46);
