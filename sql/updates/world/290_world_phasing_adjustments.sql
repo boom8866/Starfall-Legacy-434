1c9c65b9c57683668483bacf9a18443db5a35dd6
john2308 <gianni.elio@hotmail.it>
2015-06-08 14:16:28 +0200

    DB/Phasings: Adjust some phasing id's to prevent problems with player visibility
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/290_world_phasing_adjustments.sql
@@ -0,0 +1,4 @@
+UPDATE `phase_definitions` SET `phaseId`=0 WHERE `zoneId`=4714 AND `entry`=1;
+UPDATE `phase_definitions` SET `phaseId`=0 WHERE `zoneId`=616 AND `entry`=1;
+UPDATE `phase_definitions` SET `phaseId`=0 WHERE `zoneId`=4720 AND `entry`=2;
+UPDATE `phase_definitions` SET `phaseId`=0 WHERE `zoneId`=4720 AND `entry`=1;
