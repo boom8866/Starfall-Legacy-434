2cd0f398a458a6f19933bab99a57aa95d2222606
john2308 <gianni.elio@hotmail.it>
2015-06-19 12:48:28 +0200

    DB/BoT: Add all boss HP values pre-nerf for patch 4.0.6 (Bastion of Twilight)
    Signed-off-by: john2308 <gianni.elio@hotmail.it>

---
+++ sql/updates/world/Custom/03_world_bot_prenerf_boss_health.sql
@@ -0,0 +1,6 @@
+UPDATE `creature_template` SET `HealthModifier`=375 WHERE `entry` IN (45992, 45993);
+UPDATE `creature_template` SET `HealthModifier`=379 WHERE `entry`=44600;
+UPDATE `creature_template` SET `HealthModifier`=390 WHERE `entry`=44687;
+UPDATE `creature_template` SET `HealthModifier`=391 WHERE `entry`=43324;
+UPDATE `creature_template` SET `HealthModifier`=17.5 WHERE `entry`=43622;
+UPDATE `creature_template` SET `HealthModifier`=1.89 WHERE `entry`=44045;
