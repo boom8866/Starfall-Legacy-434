f14de5698681ff2aa68a0f871772e5b0f0e198a1
Northstrider <dreadkiller@gmx.de>
2015-09-26 15:56:01 +0200

    DB/BoT: removed non blizzlike hack...

--- sql/updates/world/313_world_bot_trash_position_chogall.sql
+++
@@ -1,5 +0,0 @@
--- Position update for Faceless Guardian (id: 45676) in zone: 5334, area: 5334
-UPDATE `creature` SET `position_x` = -1152.368, `position_y` = -741.016, `position_z` = 835.030, `orientation`= 1.356 WHERE `guid` = 779074;
-
--- Position update for Faceless Guardian (id: 45676) in zone: 5334, area: 5334
-UPDATE `creature` SET `position_x` = -1169.961, `position_y` = -740.969, `position_z` = 835.030, `orientation`= 1.517 WHERE `guid` = 779436;
